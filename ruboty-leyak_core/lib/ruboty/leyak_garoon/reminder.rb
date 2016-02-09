9b5b8b00c3b31ae4b2e152c8c1fc8c444db1aab8
ru_shalm <ru_shalm@hazimu.com>
2016-02-09 14:29:53 +0900

    JSON!!

--- ruboty-leyak_core/lib/ruboty/leyak_garoon/reminder.rb
+++ ruboty-leyak_core/lib/ruboty/leyak_garoon/reminder.rb
@@ -30,13 +30,14 @@ module Ruboty
         return unless Crawler.events
 
         now = Time.now
-        events = Crawler.events.select do |data|
-          event = Ruboty::LeyakGaroon::Entities::Event.new(data)
+        events = Crawler.events.map { |data|
+          Ruboty::LeyakGaroon::Entities::Event.new(data)
+        }.select { |event|
           next if event.all_day?
           next unless event.start_time > last_checked_at
           next unless (event.start_time - now) < remind_sec
           true
-        end
+        }
         if events.size > 0
           @robot.receive(
               body: "/remind-message @#{remind_owner} もうすぐ時間だよ\n```\n#{events.join("\n")}\n```",
