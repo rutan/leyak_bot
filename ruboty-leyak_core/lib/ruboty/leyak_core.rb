94a723d22aa3c21486a30c97e9de9ed07872e1c7
ru_shalm <ru_shalm@hazimu.com>
2016-12-16 01:59:16 +0900

    remove is 何

--- ruboty-leyak_core/lib/ruboty/leyak_core.rb
+++ ruboty-leyak_core/lib/ruboty/leyak_core.rb
@@ -4,6 +4,5 @@ require 'ruboty/leyak_core/version'
 
 require 'ruboty/leyak_garoon/handler' if ENV['IGNORE_GAROON'].to_i == 0
 require 'ruboty/leyak_todo/handler'
-require 'ruboty/leyak_is_nani/handler'
 
 require 'ruboty/leyak_core/handler'
