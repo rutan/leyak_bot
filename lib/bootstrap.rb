713a5d518ca1fdb2d1580f358fadb9d9d9ee537b
ru_shalm <ru_shalm@hazimu.com>
2017-08-12 18:47:18 +0900

    オートロード対象外だったので明示的に読むように

--- lib/bootstrap.rb
+++ lib/bootstrap.rb
@@ -8,6 +8,9 @@ LIB_PATH = File.expand_path('../', __FILE__)
 $LOAD_PATH.unshift LIB_PATH
 ActiveSupport::Dependencies.autoload_paths << LIB_PATH
 
+# utils
+require_relative './utils/notify_action.rb'
+
 # entities / handlers
 %w[entities handlers].each do |dir|
   Dir.glob("#{LIB_PATH}/#{dir}/**/*.rb").sort.each { |path| require(path) }
